55f842aa1d1e5004606c9794cb3a2117cc7ec85f
Glitch (tudorel) <none>
2019-04-17 16:22:16 +0000

    🙏👾 Checkpoint ./server.js:3405042/1834

--- server.js
+++ server.js
@@ -363,18 +363,25 @@ if (command == "unmute") { // creates the command unmute
         message.channel.send(sayMessage);
 
     }
+    
+        
 
     if (command === "stats") {
    var drq = new Discord.RichEmbed()
-  .addField("Proceses", `**NodeJS**: ${process.version}\n**Discord.JS**: ${require('discord.js').version}`)
-   .setColor(1337)
-   .addBlankField()
-  .addField("Servers and Users", `**Servers**: ${bot.guilds.size} **Users**: ${bot.users.size}`)
-  .addField("Ram Usage" , Math.round(process.memoryUsage().heapUsed /512/512) + "/512 MB", true)
-  .addField("Ping" , `${bot.pings[0]}`)
-  .setFooter("Comanda executata de " + message.author.username, message.author.avatarURL)
-   .addField("Owner:", "<@482833730486927381>")
-   .addField("Cpu Usage:", "0.0%")
+ .setAuthor(bot.user.username)
+            .setTitle("***BOT Stats***")
+            .setColor("RANDOM")
+            .addField("• Mem Usage", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, true)
+            .addField("• Uptime ", `${duration}`, true)
+            .addField("• Users", `${bot.users.size.toLocaleString()}`, true)
+            .addField("• Servers", `${bot.guilds.size.toLocaleString()}`, true)
+            .addField("• Channels ", `${bot.channels.size.toLocaleString()}`, true)
+            .addField("• Discord.js", `v${version}`, true)
+            .addField("• Node", `${process.version}`, true)
+            .addField("• CPU", `\`\`\`md\n${os.cpus().map(i => `${i.model}`)[0]}\`\`\``)
+            .addField("• CPU usage", `\`${percent.toFixed(2)}%\``, true)
+            .addField("• Arch", `\`${os.arch()}\``, true)
+            .addField("• Platform", `\`\`${os.platform()}\`\``, true)
    
   return message.channel.send(drq)
 
