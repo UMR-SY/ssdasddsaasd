7384b2997895b8d7c27f5b18bb4058b5396c11a1
Glitch (tudorel) <none>
2019-04-20 11:09:09 +0000

    🐂🌉 Checkpoint ./server.js:2680663/3194

--- server.js
+++ server.js
@@ -105,9 +105,23 @@ if(args[1] == "idle") return bot.user.setStatus("idle");
 
     if (command == "ping") { // creates a command *ping
       var pula = new Discord.RichEmbed()
-        message.channel.send(`🏓 Pong! | Ping: \`${bot.pings[0]} tick/ms\` `)
-    
+        message.channel.send(`🏓 Pong! | Ping: \`${bot.pings[0]} ms`)
 	}
+  if(command == "purge") {
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('Sorry, but you do not have the **Manage Messages** permissions! If you think this is an error, contact an owner.')
+    //if (!message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) return message.channe
+
+    if (!args[0]) return message.channel.send('You must specify a number of messages.');
+    if (args[0] < 1) return message.channel.send('Please provide a number greater than 1')
+    if (args[0] > 100) return message.channel.send('Please provide a number less than 100.');
+    if (isNaN(args[0])) return message.channel.send('Please provide a corect number.');
+
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`🗑 I delete **${args[0]}** messages.`).then(message => message.delete(3000));
+    }).catch().catch((e) => message.channel.send('You can not delete messages older than 14 days.'));
+
+
+}
 
 
 	  if (command == "avatar") {
