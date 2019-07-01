c0c747dd6b84349a5cc325bb03725c41ddc2089f
Glitch (tudorel) <none>
2019-07-01 18:13:00 +0000

    👷🐆 Checkpoint ./server.js:3405042/78214 ./settings.json:3405042/268 ./jewdev.js:3405042/58661 ./package.json:3405042/1621 ./package-lock.json:3405042/36016 ./cofnig.json:3405042/338 ./commands/fortnite.js:3405042/1721 ./commands/help.js:3405042/1114 ./accounts/fortnite.txt:3405042/69

---
+++ commands/fortnite.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const cooldown = new Set();
+
+module.exports.run = async (client, msg, args, config) => {
+    if(cooldown.has(msg.author.id)) {
+        msg.reply(`You need to wait ${config.COOLDOWN} minutes to use this command again!`)
+            .then((m) => {
+                msg.delete();
+
+                setTimeout(() => {
+                    m.delete();
+                }, 5000);
+            });
+    } else {
+        fs.readFile('./accounts/fortnite.txt', 'utf8', function(err, data) {
+            if (err) throw err;
+
+            data = data + '';
+            var lines = data.split('\n');
+            let account = lines[Math.floor(Math.random() * 1)];
+
+            fs.writeFile('./accounts/fortnite.txt', lines.slice(1).join('\n'), function(err) {
+                if(err) throw err;
+            });
+
+            let embed = new Discord.RichEmbed()
+            .addField('Fortnite account', `Random account (email:password): \n**${account}**`)
+            .setThumbnail('https://i.imgur.com/09Fxrfw.png')
+            .setColor('#FFFFFF')
+            .setFooter('Bot made by jewdev')
+            .setTimestamp();
+
+            msg.author.send(embed);
+
+            msg.reply('I\'ve sent you the account! Please check your DM!')
+                .then(m => {
+                    setTimeout(() => {
+                        m.delete();
+                    }, 5000);
+                });
+
+            cooldown.add(msg.author.id);
+            setTimeout(() => {
+                cooldown.delete(msg.author.id);
+            }, config.COOLDOWN * 60 * 1000);
+        });
+    }
+};
+
+module.exports.help = {
+    name: `fortnite`,
+    description: `Sends you a Fortnite account!`
+};
---
+++ commands/help.js
@@ -0,0 +1,34 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, msg, args, config) => {
+    let embed = new Discord.RichEmbed()
+        .setColor('#E09099')
+        .setDescription(`
+Type **${config.PREFIX}help <command>** for more information about a command.
+For arguments <> means required, [] means optional.`)
+        .addField('Commands', client.commands.map(x => `\`${x.help.name}\``).join(', '))
+        .setFooter(`Bot made by jewdev | Ping: ${client.ping}`)
+        .setTimestamp();
+
+        if(!args.join(' ')) return msg.channel.send(embed);
+
+        let searchCmd = args.join(' ');
+        if(client.commands.has(searchCmd)) {
+            searchCmd = client.commands.get(searchCmd);
+
+            let helpEmbed = new Discord.RichEmbed()
+            .setColor('#E09099')
+            .setDescription(`
+**Name:** ${searchCmd.help.name}
+**Description:** ${searchCmd.help.description}`);
+
+            msg.channel.send(helpEmbed);
+        } else {
+            msg.reply('This command doesn\'t exists.');
+        }
+};
+
+module.exports.help = {
+    name: `help`,
+    description: `Bot's commands`
+};
