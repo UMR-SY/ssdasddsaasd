f8ee85a0383b95ce762fc7b62b04a9c506942ab6
Glitch (tudorel) <none>
2019-11-19 16:05:53 +0000

    🐧🍳 Checkpoint ./accounts/mc.txt:3405042/1473 ./commands/minecraft.js copy:3405042/1791

---
+++ commands/minecraft.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const cooldown = new Set();
+
+module.exports.run = async (client, msg, args, config) => {
+    if(cooldown.has(msg.author.id)) {
+        msg.reply(`You need to wait ${config.COOLDOWN} minutes to use this command again!`)
+            .then((m) => {
+                msg.delete();
+
+                setTimeout(() => {
+                    m.delete();
+                }, 5000);
+            });
+    } else {
+        fs.readFile('./accounts/spotify.txt', 'utf8', function(err, data) {
+            if (err) throw err;
+
+            data = data + '';
+            var lines = data.split('\n');
+            let account = lines[Math.floor(Math.random() * 1)];
+
+            fs.writeFile('./accounts/spotify.txt', lines.slice(1).join('\n'), function(err) {
+                if(err) throw err;
+            });
+
+            let embed = new Discord.RichEmbed()
+            .addField('Support Server', `https://discord.gg/xAQPrGc`)
+            .addField('Spotify account',`\n**${account}**`)
+            .setThumbnail('https://cdn-images-1.medium.com/max/2400/1*BKoo1Q5PBuN87XT4bArK3w.jpeg')
+            .setColor('#FFFFFF')
+            .setFooter('Bot made by MRLIT_')
+            .setTimestamp();
+
+            msg.author.send(embed);
+
+            msg.reply('I\'ve sent you the account! Please check your DM!')
+                .then(m => {
+                    setTimeout(() => {
+                        m.delete();
+                    }, 5000);
+                });
+
+            cooldown.add(msg.author.id);
+            setTimeout(() => {
+                cooldown.delete(msg.author.id);
+            }, config.COOLDOWN * 60 * 1000);
+        });
+    }
+};
+
+module.exports.help = {
+    name: `spotify`,
+    description: `Sends you a Spotify account!`
+};
