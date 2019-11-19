336ed5aa395e591b43e6df048636a60b402d320e
Glitch (tudorel) <none>
2019-11-19 14:42:55 +0000

    🎢📻 Checkpoint ./commands/spotify.js:3405042/150 ./accounts/spotify.txt:3405042/2071

--- commands/spotify.js
+++ commands/spotify.js
@@ -25,7 +25,8 @@ module.exports.run = async (client, msg, args, config) => {
             });
 
             let embed = new Discord.RichEmbed()
-            .addField('Spotify account', `Random account (email:password): \n**${account}**`)
+            .addField('Support Server', `https://discord.gg/xAQPrGc`)
+            .addField('Spotify account',`\n**${account}**`)
             .setThumbnail('https://cdn-images-1.medium.com/max/2400/1*BKoo1Q5PBuN87XT4bArK3w.jpeg')
             .setColor('#FFFFFF')
             .setFooter('Bot made by MRLIT_')
