1700f41c761926d9409cf394f8b58d86f67c7d17
Glitch (tudorel) <none>
2019-11-21 15:54:05 +0000

    🛍🐟 Checkpoint ./commands/disney.js:3405042/2205 ./accounts/disney.js:3405042/7114 ./accounts/disney.txt:3405042/82677

---
+++ commands/disney.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const cooldown = new Set();
+
+module.exports.run = async (client, msg, args, config) => {
+    if(cooldown.has(msg.author.id)) {
+        msg.reply(`You need to wait ${config.COOLDOWN} minutes to use this command again!`)
+            .then((m) => {
+                msg.delete();
+
+                setTimeout(() => {
+                    m.delete();
+                }, 5000);
+            });
+    } else {
+        fs.readFile('./accounts/disney.txt', 'utf8', function(err, data) {
+            if (err) throw err;
+
+            data = data + '';
+            var lines = data.split('\n');
+            let account = lines[Math.floor(Math.random() * 1)];
+
+            fs.writeFile('./accounts/disney.txt', lines.slice(1).join('\n'), function(err) {
+                if(err) throw err;
+            });
+
+            let embed = new Discord.RichEmbed()
+            .addField('Support Server', `https://discord.gg/xAQPrGc`)
+            .addField('Disney account',`\n**${account}**`)
+            .setThumbnail('https://www.google.ro/url?sa=i&source=images&cd=&ved=2ahUKEwi10LeL0_vlAhVELFAKHRFLDcUQjRx6BAgBEAQ&url=https%3A%2F%2Fvariety.com%2F2019%2Ftv%2Fnews%2Fdisney-appoints-jan-koeppen-to-lead-europe-middle-east-africa-1203354626%2F&psig=AOvVaw3fgIH6_8gzJ1pxWYwUWNaE&ust=1574437549681200')
+            .setColor('#FFFFFF')
+            .setFooter('Bot made by MRLIT_')
+            .setTimestamp();
+
+            msg.author.send(embed);
+
+            msg.reply('I\'ve sent you the account! Please check your DM!')
+                .then(m => {
+                    setTimeout(() => {
+                        m.delete();
+                    }, 5000);
+                });
+
+            cooldown.add(msg.author.id);
+            setTimeout(() => {
+                cooldown.delete(msg.author.id);
+            }, config.COOLDOWN * 60 * 1000);
+        });
+    }
+};
+
+module.exports.help = {
+    name: `disney`,
+    description: `Sends you a Disney account!`
+};
