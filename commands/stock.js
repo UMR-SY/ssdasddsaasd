f9a9ffbbe3d5369f4368c66f5bd0b4da270fdbf5
Glitch (tudorel) <none>
2019-11-24 18:07:55 +0000

    🎆👧 Checkpoint ./commands/stock.js:3405042/526

--- commands/stock.js
+++ commands/stock.js
@@ -2,20 +2,15 @@ const Discord = require ("discord.js")
 
 exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
-  .setTitle('D-GEN STOCKS')
-  .setCOLOR('R')
-  .addField("DISNEY ACCOUNTS")
-  .addField("Available: 1800 | !disney")
-    .addField("MINECRAFT ACCOUNTS")
-  .addField("Available: 258 | !minecraft")
-    .addField("SPOTIFY ACCOUNTS")
-  .addField("Available: 320 | !spotify")
-    .addField("HULU ACCOUNTS")
-  .addField("Available: 2320 | !hulu")
-    .addField("NORDVPN ACCOUNTS")
-  .addField("Available: 1850 | !nordvpn")
-  .setFooter('D-GEN')
-message.channel.send(embed)
+  .setColor('RANDOM')
+  .addField('DISNEY ACCOUNTS', 'Available: 1800 | !disney')
+    .addField('MINECRAFT ACCOUNTS', 'Available: 258 | !minecraft')
+    .addField('SPOTIFY ACCOUNTS', 'Available: 320 | !spotify')
+    .addField('HULU ACCOUNTS', 'Available: 2320 | !hulu')
+    .addField('NORDVPN ACCOUNTS', 'Available: 1850 | !nordvpn')
+  .setFooter('D-GEN STOCK')
+   .setTimestamp();
+ return message.channel.send(embed)
 }
 module.exports.help = {
     name: `stock`,
