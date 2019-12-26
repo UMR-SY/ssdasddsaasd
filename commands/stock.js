6f3bf275802db1bb7df0974944ae59ea863f5d8c
Glitch (tundra-timimus) <none>
2019-12-26 13:03:24 +0000

    🏄🏈 Checkpoint ./accounts/fortnite.txt:8577455/53037 ./commands/stock.js:8577455/5

--- commands/stock.js
+++ commands/stock.js
@@ -17,7 +17,7 @@ __**Stock Accounts**__`)
         .addField('**✩** `$nordvpn` 1500 **.**', "⠀")
         .addField('**✩** `$proxy` unlimited**.**', "⠀")
         .addField('**✩** `$shein`  1200**.**', "⠀")
-        .addField('**✩** `$fortnite`  1500**.**', "⠀")
+        .addField('**✩** `$fortnite`  0**.**', "⠀")
         .addField('**✩** `$spotify`  5000**.**', "⠀")
         .setTimestamp();
 
