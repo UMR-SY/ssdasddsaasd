8a86694f5a40be99632c0e448c50431c9ab38711
Glitch (tundra-timimus) <none>
2019-12-26 12:15:27 +0000

    🌵📖 Checkpoint ./commands/stock.js:3405042/224:8577455/363

--- commands/stock.js
+++ commands/stock.js
@@ -6,8 +6,16 @@ exports.run = async (client, message, args, level) => {
   let embed = new Discord.RichEmbed()
   
 .setTitle("Stock")
+  .addField("**Origin**: `1600`", "")// st ca bag eu comanda de dmall pe dal drq tot dau afk:)))))
+  
+  
+  
+  
+  
+
+  
   .setColor('RANDOM')
-.addField("**Origin**: `1600`", "")
+
   
   return message.channel.send(embed)
   }
