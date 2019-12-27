68e2ec2952c5a2a845bd36dfca1f726da4660f2f
Glitch (tundra-timimus) <none>
2019-12-27 18:36:03 +0000

    🌠👟 Checkpoint ./commands/test.js:8577455/1214

--- commands/help.js
+++ commands/help.js
@@ -1,47 +1,12 @@
-const Discord = require('discord.js');
+const Discord = require ("discord.js")
 
-module.exports.run = async (client, msg, args, config) => {
-    let embed = new Discord.RichEmbed()
-        .setColor("#363940")
-        .setTitle('Help')
-        .setURL('https://discordapp.com/api/oauth2/authorize?client_id=659451089266933760&permissions=8&scope=bot')
-        .setDescription(`
-__**GENERATOR COMMANDS**__`)
-        .addField('⠀', "⠀")
-        .addField('**✩** `$minecraft` To Get A Minecraft Account **.**', "⠀")
-        .addField('**✩** `$disney` To Get A Disney Account **.**', "⠀")
-        .addField('**✩** `$hulu` To Get A Hulu Account **.**', "⠀")
-        .addField('**✩** `$origin` To Get A Origin Account **.**', "⠀")
-        .addField('**✩** `$cbs` To Get A Cbs Account **.**', "⠀")
-        .addField('**✩** `$nitro` To Get A Unchecked Code Account **.**', "⠀")
-        .addField('**✩** `$nordvpn` To Get A NordVPN Account **.**', "⠀")
-        .addField('**✩** `$proxy` To Get A Proxy **.**', "⠀")
-        .addField('**✩** `$shein` To Get A Shein Account **.**', "⠀")
-        .addField('**✩** `$spotify` To Get A Spotify Account **.**', "⠀")
-        .addField('⠀', "⠀")
-        .addField('⠀', "**✩** Remember that you are using the free generator, this accounts may not work !")
-        .setFooter(`Bot made by Norkez | Ping: ${client.ping}`)
-        .setTimestamp();
-
-        if(!args.join(' ')) return msg.channel.send(embed);
-
-        let searchCmd = args.join(' ');
-        if(client.commands.has(searchCmd)) {
-            searchCmd = client.commands.get(searchCmd);
-
-            let helpEmbed = new Discord.RichEmbed()
-            .setColor('#000000')
-            .setDescription(`
-**Name:** ${searchCmd.help.name}
-**Description:** ${searchCmd.help.description}`);
-
-            msg.channel.send(helpEmbed);
-        } else {
-            msg.reply('This command doesn\'t exists.');
-        }
-};
-
+exports.run = async (client, message) => {
+  let embed = new Discord.RichEmbed()
+.setColor("#363940")
+.setFooter('©️ CopyRight Genesis™️')
+.addField('__Help__','__**GENERATOR COMMANDS**__\n\n`$fortnite` To Get A Fortnite Account .\n`$spotify` To Get A Spotify Account .\n`$cbs` To Get A cbs Account .\n`$disney` To Get A Disney Account.\n`$hulu` To Get A Hulu Account .\n`$minecraft` To Get A Minecraft Account .\n`$nitro` To Get A Nitro Code .\n`$nordvpn` To Get A NordVPN Account .\n`$origin` To Get A Origin Account .\n`$proxy` To Get A Proxy .\n`$shein` To Get A Shein Account .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
+message.channel.send(embed)
+}
 module.exports.help = {
-    name: `help`,
-    description: `Bot's commands`
-};
+  name: 'help'
+}
--- commands/test.js
+++ commands/test.js
@@ -2,9 +2,11 @@ const Discord = require ("discord.js")
 
 exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
-.addField('Help','__**GENERATOR COMMANDS**__\n\n`$fortnite` To Get A Fortnite Account .\n`$spotify` To Get A Spotify Account .\n`$cbs` To Get A cbs Account .\n`$disney` To Get A Disney Account.\n`$hulu` To Get A Hulu Account .\n`$minecraft` To Get A Minecraft Account .\n`$nitro` To Get A Nitro Code .\n`$NordVPN` To Get A NordVPN Account .\n`$origin` To Get A Origin Account .\n`$proxy` To Get A Proxy .\n`$shein` To Get A Shein Account .\n\n');
+.setColor("#363940")
+.setFooter('©️ CopyRight Genesis™️')
+.addField('__Stock__','__**STOCK ACCOUNTS**__\n\n`$fortnite` To Get A Fortnite Account .\n`$spotify` To Get A Spotify Account .\n`$cbs` To Get A cbs Account .\n`$disney` To Get A Disney Account.\n`$hulu` To Get A Hulu Account .\n`$minecraft` To Get A Minecraft Account .\n`$nitro` To Get A Nitro Code .\n`$nordvpn` To Get A NordVPN Account .\n`$origin` To Get A Origin Account .\n`$proxy` To Get A Proxy .\n`$shein` To Get A Shein Account .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
 message.channel.send(embed)
 }
 module.exports.help = {
-  name: 'test'
+  name: 'help'
 }
