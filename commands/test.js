487aa5e31bb083ca22f2f2f06a253cc89ed5b808
Glitch (tundra-timimus) <none>
2019-12-27 18:59:06 +0000

    🐄🚦 Checkpoint ./commands/test.js:8577455/336

--- commands/test.js
+++ commands/test.js
@@ -4,7 +4,7 @@ exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
 .setColor("#363940")
 .setFooter('©️ CopyRight Genesis™️')
-.addField('__Stock__','__**STOCK ACCOUNTS**__\n\n`$fortnite`: 0 .\n`$spotify`: 1975 .\n`$cbs` To Get A cbs Account .\n`$disney` To Get A Disney Account.\n`$hulu` To Get A Hulu Account .\n`$minecraft` To Get A Minecraft Account .\n`$nitro` To Get A Nitro Code .\n`$nordvpn` To Get A NordVPN Account .\n`$origin` To Get A Origin Account .\n`$proxy` To Get A Proxy .\n`$shein` To Get A Shein Account .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
+.addField('__Stock__','__**STOCK ACCOUNTS**__\n\n`$fortnite`: 0 .\n`$spotify`: 1975 .\n`$cbs`: 1705 .\n`$disney`: 948.\n`$hulu`: 2049 .\n`$minecraft`: 1203 .\n`$nitro`: unlimited .\n`$nordvpn`: 2602.\n`$origin` 863.\n`$proxy`: unlimited .\n`$shein`: 1592 .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
 message.channel.send(embed)
 }
 module.exports.help = {
