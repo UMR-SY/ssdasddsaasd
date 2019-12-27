796b5e6e1a0851375870131277c8b18a59b5eb30
Glitch (tundra-timimus) <none>
2019-12-27 15:54:11 +0000

    🙏🎐 Checkpoint ./accounts/origin.txt:8577455/60551 ./commands/test.js:3405042/763:8577455/98

---
+++ commands/test.js
@@ -0,0 +1,12 @@
+const Discord = require ("discord.js")
+
+exports.run = async (client, message) => {
+  let embed = new Discord.RichEmbed()
+.addField('Help','Gen commands\n`$fortnite` : To Get A Fortnite Account .\n`$spotify` :To Get A Spotify Account .');
+message.channel.send(embed)
+}
+module.exports.help = {
+  name: 'test'
+}
+
+
