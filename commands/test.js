3aa98952d3c65574016a00b97346e8076b42ea3f
Glitch (tundra-timimus) <none>
2019-12-27 16:06:11 +0000

    🎎👨‍❤️‍💋‍👨 Checkpoint ./commands/test.js:3405042/717:8577455/6182

--- commands/test.js
+++ commands/test.js
@@ -2,7 +2,7 @@ const Discord = require ("discord.js")
 
 exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
-.addField('Help','Gen commands\n`$fortnite` : To Get A Fortnite Account .\n`$spotify` :To Get A Spotify Account .');
+.addField('Help','Gen commands\n`$fortnite` To Get A Fortnite Account .\n`$spotify` To Get A Spotify Account .');
 message.channel.send(embed)
 }
 module.exports.help = {
@@ -10,3 +10,23 @@ module.exports.help = {
 }
 
 
+// ce
+// o sa vezi
+// zi
+// stii cum era bot ul ala de gen pt toti? gen sa bage ei cotnuri
+// asa fac eu acm
+// iei
+// anunt ca o sa fie update?
+// =)))))))))
+// DEJA AI DAT UPDATE =)))))))))))))))))))))))))))))
+// la canale
+// le opresc genu? pana faci?
+// fac alt bot maa :))))
+// gen unu separat
+
+//  facem gen cu numele Genesis (ALL)
+// :))))))
+// ok sa imi dai inv in project nou k?
+// afk bag stock uplay
+// afk fuck
+// eu am duc sa vad daca imi iese
