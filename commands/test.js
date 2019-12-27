45a8ca90f24fdd0e65cc716ed08dfbe71cbe577b
Glitch (tundra-timimus) <none>
2019-12-27 16:17:44 +0000

    🏖⛹ Checkpoint ./commands/test.js:8577455/458

--- commands/test.js
+++ commands/test.js
@@ -8,25 +8,3 @@ message.channel.send(embed)
 module.exports.help = {
   name: 'test'
 }
-
-
-// ce
-// o sa vezi
-// zi
-// stii cum era bot ul ala de gen pt toti? gen sa bage ei cotnuri
-// asa fac eu acm
-// iei
-// anunt ca o sa fie update?
-// =)))))))))
-// DEJA AI DAT UPDATE =)))))))))))))))))))))))))))))
-// la canale
-// le opresc genu? pana faci?
-// fac alt bot maa :))))
-// gen unu separat
-
-//  facem gen cu numele Genesis (ALL)
-// :))))))
-// ok sa imi dai inv in project nou k?
-// afk bag stock uplay
-// afk fuck
-// eu am duc sa vad daca imi iese
