0fee1863e7b6003cc78afbc2dc1fb57014d82728
Glitch (tundra-timimus) <none>
2019-12-29 17:10:37 +0000

    💍🐴 Checkpoint ./accounts/fortnite.txt:8577455/12961 ./commands/stock.js:8577455/4

--- commands/stock.js
+++ commands/stock.js
@@ -5,7 +5,7 @@ exports.run = async (client, message) => {
 .setColor("#363940")
 .setThumbnail('https://cdn.discordapp.com/attachments/659802254680916008/659870685971218462/Project_Genesis_2.png')
 .setFooter('©️ CopyRight Genesis™️')
-.addField('__Stock__','__**STOCK ACCOUNTS**__\n\n`$fortnite`: 0 .\n`$spotify`: 6400 .\n`$cbs`: 1705 .\n`$disney`: 948.\n`$hulu`: 2049 .\n`$minecraft`: 1203 .\n`$nitro`: unlimited .\n`$nordvpn`: 2602.\n`$origin` 863.\n`$proxy`: unlimited .\n`$shein`: 1592 .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
+.addField('__Stock__','__**STOCK ACCOUNTS**__\n\n`$fortnite`: 334 .\n`$spotify`: 6400 .\n`$cbs`: 1705 .\n`$disney`: 948.\n`$hulu`: 2049 .\n`$minecraft`: 1203 .\n`$nitro`: unlimited .\n`$nordvpn`: 2602.\n`$origin` 863.\n`$proxy`: unlimited .\n`$shein`: 1592 .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
 message.channel.send(embed)
 }
 module.exports.help = {
