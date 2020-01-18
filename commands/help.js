65d3bd98297bc0d124ab7dd5b4f9dc0be05c2618
Glitch (tundra-timimus) <none>
2020-01-18 15:13:08 +0000

    📅🚓 Checkpoint ./commands/stock.js:8577455/40 ./accounts/vpn.txt:8577455/94079 ./accounts/shein.txt:8577455/148095 ./accounts/cbs.txt:8577455/93162 ./commands/cbs.js:8577455/434 ./commands/disney.js:8577455/910 ./commands/help.js:8577455/268 ./commands/hulu.js:8577455/754 ./commands/minecraft.js:8577455/1416

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,8 @@ const Discord = require ("discord.js")
 
 exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
-.setColor("#363940")
-.setThumbnail('https://cdn.discordapp.com/attachments/659802254680916008/659870685971218462/Project_Genesis_2.png')
+.setColor("#363940")       
+.setThumbnail('https://cdn.discordapp.com/attachments/668055807857786891/668110267804286986/bb4eac1d-40b0-406f-996a-7e8bc3d3fbda.png')
 .setFooter('©️ CopyRight Omega™️')
 .addField('__Help__','__**GENERATOR COMMANDS**__\n\n`$spotify` To Get A Spotify Account .\n`$cbs` To Get A cbs Account .\n`$disney` To Get A Disney Account.\n`$hulu` To Get A Hulu Account .\n`$minecraft` To Get A Minecraft Account .\n`$nitro` To Get A Nitro Code .\n`$nordvpn` To Get A NordVPN Account .\n`$origin` To Get A Origin Account .\n`$proxy` To Get A Proxy .\n`$shein` To Get A Shein Account .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
 message.channel.send(embed)
--- commands/stock.js
+++ commands/stock.js
@@ -4,7 +4,7 @@ exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
 .setColor("#363940")
 .setFooter('©️ CopyRight Omega™️')
-.addField('__STOCK__','__**STOCK COMMANDS**__\n\n`$spotify`: 3203 .\n`$cbs`: 0 .\n`$disney`: 1001.\n`$hulu`: 2049 .\n`$minecraft`: 200 .\n`$nitro`: unlimited .\n`$nordvpn`: 2602.\n`$origin` 863.\n`$proxy`: unlimited .\n`$shein`: 1592 .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
+.addField('__STOCK__','__**STOCK COMMANDS**__\n\n`$spotify`: 3203 .\n`$cbs`: 1459 .\n`$disney`: 1001.\n`$hulu`: 650 .\n`$minecraft`: 0 .\n`$nitro`: unlimited .\n`$nordvpn`: 1200.\n`$origin` 5600.\n`$proxy`: unlimited .\n`$shein`: 500 .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
 message.channel.send(embed)
 }
 module.exports.help = {
