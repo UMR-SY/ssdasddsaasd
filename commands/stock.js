13b3e72dd0e060080df2236c13384bfc52e1b1f1
Glitch (tundra-timimus) <none>
2020-01-18 15:01:08 +0000

    🙌⭐️ Checkpoint ./accounts/origin.txt:8577455/182547 ./commands/stock.js:8577455/24 ./accounts/disney.txt:8577455/118316

--- commands/stock.js
+++ commands/stock.js
@@ -4,7 +4,7 @@ exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
 .setColor("#363940")
 .setFooter('©️ CopyRight Omega™️')
-.addField('__STOCK__','__**STOCK COMMANDS**__\n\n`$spotify`: 1450 .\n`$cbs`: 1705 .\n`$disney`: 948.\n`$hulu`: 2049 .\n`$minecraft`: 200 .\n`$nitro`: unlimited .\n`$nordvpn`: 2602.\n`$origin` 863.\n`$proxy`: unlimited .\n`$shein`: 1592 .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
+.addField('__STOCK__','__**STOCK COMMANDS**__\n\n`$spotify`: 3203 .\n`$cbs`: 0 .\n`$disney`: 1001.\n`$hulu`: 2049 .\n`$minecraft`: 200 .\n`$nitro`: unlimited .\n`$nordvpn`: 2602.\n`$origin` 863.\n`$proxy`: unlimited .\n`$shein`: 1592 .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
 message.channel.send(embed)
 }
 module.exports.help = {
