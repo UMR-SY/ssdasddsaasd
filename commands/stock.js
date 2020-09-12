2abab8ab3797178e97a38e9e2c72ea50a4322129
Glitch (genbot-tutorial) <none>
2020-09-12 15:47:55 +0000

    😽🚒 Checkpoint ./config.json:22109255/4 ./commands/stock.js:22109255/1413 ./commands/spotify.js:22109255/237

--- commands/stock.js
+++ commands/stock.js
@@ -1,12 +1,15 @@
-const Discord = require ("discord.js")
+const Discord = require("discord.js");
 
 exports.run = async (client, message) => {
   let embed = new Discord.RichEmbed()
-.setColor("#363940")
-.setFooter('©️ CopyRight Omega™️')
-.addField('__STOCK__','__**STOCK COMMANDS**__\n\n`$spotify`: 3203 .\n`$cbs`: 1459 .\n`$disney`: 1001.\n`$hulu`: 650 .\n`$minecraft`: 0 .\n`$nitro`: unlimited .\n`$nordvpn`: 1200.\n`$origin` 5600.\n`$proxy`: unlimited .\n`$shein`: 500 .\n\n **✩** Remember that you are using the free generator, this accounts may not work !');
-message.channel.send(embed)
-}
+    .setColor("#363940")
+    .setFooter("©️ Narcos Gen™️")
+    .addField(
+      "__STOCK__",
+      "__**Narcos Gen Stokları**__\n\n`!spotify`: 3203 .\n`!cbs`: 1459 .\n`!disney`: 1001.\n`!hulu`: 650 .\n`!minecraft`: 0 .\n`!nitro`: unlimited .\n`!nordvpn`: 1200.\n`!origin` 5600.\n`!proxy`: unlimited .\n`!shein`: 500 .\n\n **✩** Ücretsiz oluşturucuyu kullandığınızı unutmayın, bu hesaplar çalışmayabilir!"
+    );
+  message.channel.send(embed);
+};
 module.exports.help = {
-  name: 'help'
-}
+  name: "help"
+};
